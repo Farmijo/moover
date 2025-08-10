1a94f91dd699cf3831c081fc10be3bffbdd20601
Fran <farmijo16@gmail.com>
2025-08-10 18:27:59 +0200

    less padding on layout

--- moover-frontend/src/app/dashboard/layout.tsx
+++ moover-frontend/src/app/dashboard/layout.tsx
@@ -59,7 +59,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
       </nav>
 
       {/* Main content */}
-      <main className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <main className="max-w-7xl mx-auto py-2 sm:px-6 lg:px-8">
         <div className="px-4 py-6 sm:px-0">
           {children}
         </div>
