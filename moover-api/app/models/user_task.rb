042d3965313738cf64f72e504ec1c7e24246d158
Fran <farmijo16@gmail.com>
2025-08-10 18:30:56 +0200

    Move status on completed task

--- moover-api/app/models/user_task.rb
+++ moover-api/app/models/user_task.rb
@@ -17,6 +17,9 @@ class UserTask < ApplicationRecord
   # Callback para marcar como completada
   before_save :set_completed_at, if: :completed_changed?
   
+  # Callback para cambiar el estado de la mudanza cuando se complete la primera tarea
+  after_save :update_move_status_if_first_task_completed, if: :saved_change_to_completed?
+  
   # Helper methods
   def overdue?
     !completed? && due_date && due_date < Date.current
@@ -46,6 +49,24 @@ class UserTask < ApplicationRecord
       self.completed_at = Time.current unless completed_at
     else
       self.completed_at = nil
+    end
+  end
+  
+  def update_move_status_if_first_task_completed
+    if completed?
+      # Tarea se acaba de completar
+      # Solo cambiar si la mudanza está en estado de planificación
+      if move.planning?
+        move.update_column(:status, Move.statuses[:in_progress])
+        Rails.logger.info "Mudanza ##{move.id} cambió a estado 'in_progress' al completar la primera tarea"
+      end
+    else
+      # Tarea se acaba de desmarcar
+      # Si no hay otras tareas completadas, regresar a planificación
+      if move.in_progress? && move.user_tasks.completed.count == 0
+        move.update_column(:status, Move.statuses[:planning])
+        Rails.logger.info "Mudanza ##{move.id} regresó a estado 'planning' al no tener tareas completadas"
+      end
     end
   end
 end
