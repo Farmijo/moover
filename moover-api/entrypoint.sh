c725c75945898d0ccf7aa84480f9d0e30d9612a6
Fran <farmijo16@gmail.com>
2025-08-11 17:48:07 +0200

    entrypoing failure

--- moover-api/entrypoint.sh
+++ moover-api/entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-# Espera a que Postgres esté listo
-until PGPASSWORD=password psql -h "db" -U "moover" -d "moover_development" -c '\q'; do
-  echo "Postgres está inaccesible — esperando..."
-  sleep 1
-done
 
 # Crea y migra la base de datos
 echo "Aplicando migraciones..."
