8954632ca188349f2adc3b53f79bf8808db53958
Fran <farmijo16@gmail.com>
2025-08-11 20:38:12 +0200

    Proper cors

--- moover-api/config/initializers/cors.rb
+++ moover-api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://moover-frontend.vercel.app'
+    origins 'https://moover-omega.vercel.app'
 
     resource '*',
       headers: :any,
